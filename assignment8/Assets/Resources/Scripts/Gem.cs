fc245e9cac33d474005d960b54a07579d02afac9
aridus1 <gm2advait@gmail.com>
2020-10-12 12:51:34 +0530

    Updated Assignment 8

---
+++ assignment8/Assets/Resources/Scripts/Gem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		// Despawn if  moving past left edge
+		if (transform.position.x < -25)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			transform.Translate(-SkyscraperSpawner.speed * Time.deltaTime,
+				 0, 0, Space.World);
+		}
+
+		transform.Rotate(0, 5f, 0, Space.World);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		other.transform.parent.GetComponent<HeliController>().PickupGem();
+		Destroy(gameObject);
+	}
+}
---
+++ assignment8/Assets/Resources/Scripts/GemSpawner.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class GemSpawner : MonoBehaviour
+{
+	public GameObject[] prefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+		//infinite gem spawning function, asynchronus
+		StartCoroutine(SpawnGems());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	IEnumerator SpawnGems()
+	{
+		while (true)
+		{
+			// number of gems we could spawn vertically
+			int gemsThisRow = 1;
+
+			// instantiate all coins in this row separated by some amount of space
+			for(int i = 0; i < gemsThisRow; i++)
+			{
+				Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(26, Random.Range(-10, 10), 10), Quaternion.identity);
+			}
+
+			// pause 1-5 seconds until the next coin spawns
+			yield return new WaitForSeconds(Random.Range(4, 20));
+		}
+	}
+}
